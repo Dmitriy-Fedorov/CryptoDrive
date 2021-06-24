9118de4718db5bf270f1e495af5dbe61c08ca209
Dmitriy-Fedorov <d.d.96@yandex.ru>
2021-06-24 18:19:25 +0600

    Crypto drive e updates

--- helpers.py
+++ helpers.py
@@ -16,22 +16,6 @@ def mkdir(folder_name):
 def strip_aes(fname):
     return fname[::-1].replace(".aes"[::-1],"",1)[::-1]
 
-def driveList2localDict(dlist, id, parent, level=0, verbose=True, indent = "|---"):
-    folder_dict={"isfolder": True, 
-                 "id": id,
-                 "title": parent,
-                 "children": []}
-    for f in dlist:
-        if f["isfolder"]:
-            if verbose: print(f"{indent*level}{parent}: FOLDER {f['title']}")
-            folder_dict['children'].append(driveList2localDict(f['list'], f['id'], f['title'], level+1, verbose, indent))
-        else:
-            if verbose: print(f"{indent*level}{parent}: FILE   {f['title']}")
-            folder_dict['children'].append({"isfolder": False, 
-                                            "id": f["id"],
-                                            "title": f['title']})
-    return folder_dict
-
 
 def ignore(fname):
     if fname in ['.git', ".ipynb_checkpoints", "__pycache__"]: # , "test"
@@ -60,7 +44,7 @@ def deobfuscate(password: str, ciphertext: str) -> str:
 
 
 def size_to_upload(f2upload, in_bytes=False):
-    total = np.sum([file_size(f"{f[0]}/{f[1]}", in_bytes=True) for f in f2upload])
+    total = sum([file_size(f"{f[0]}/{f[1]}", in_bytes=True) for f in f2upload])
     if in_bytes:
         return total
     return file_size(total)
@@ -95,25 +79,3 @@ def file_size(fname, in_bytes=False, true_bytes=True):
         else:
             return f"{byte/1e9:.2f}Gb"
 
-
-
-# difference between two nested dict that represent file system structure
-# difference(local, remote)   # return files that are local but not present on server we need to upload them
-# difference(remote, local)   # return files that are present on server but not locally and we need to download them
-def difference(d2, d1, level=0):  # d2 - d1
-    d1_folders = {ch['title']: ch for ch in d1['children'] if ch['isfolder']}
-    d1_files = [ch['title'] for ch in d1['children'] if not ch['isfolder']]
-
-    d2_folders = {ch['title']: ch for ch in d2['children'] if ch['isfolder']}
-    d2_files = [ch for ch in d2['children'] if not ch['isfolder']]
-
-    # completely new folders
-    diff_folders = [folder for folder in d2_folders.values() if folder['title'] not in d1_folders.keys()]
-    # existing folder, but they are required to be checked reqursevely
-    same_folders = [folder for folder in d2_folders.keys() if folder in d1_folders.keys()]
-
-    diff_next = [difference(d2_folders[folder], d1_folders[folder], level+1) for folder in same_folders]
-    diff_files = [file for file in d2_files if file['title'] not in d1_files]
-    return {'title': d2['title'],
-            'isfolder': True,
-            'children': diff_folders + diff_files + diff_next}
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # from pydrive.auth import GoogleAuth
 # from extGoogleDrive import extGoogleDrive as GoogleDrive
-from helpers import mkdir, driveList2localDict, ignore, strip_aes
+from helpers import mkdir, ignore, strip_aes, file_size, size_to_upload
 from helpers import obfuscate, deobfuscate
 from pprint import pprint
 import hashlib
@@ -11,148 +11,156 @@ import os
 
 class DriveHandler:
     
-    def __init__(self, drive, password = "foopassword", bufferSize=64*1024, cipher=True):
-        self.root_id = "1YJkqN-qtRHXnL4GvT3icGyl05_dcYTE6"
+    def __init__(self, local_root, drive, root_id="1YJkqN-qtRHXnL4GvT3icGyl05_dcYTE6", password = "foopassword", bufferSize=64*1024, cipher=True):
+        self.root_id = root_id
+        self.local_root = local_root
         self.cache = None
         self.bufferSize = bufferSize
         self.password = hashlib.sha256(password.encode("utf-8")).hexdigest()
         self.drive = drive
         self.cipher = cipher
+        self.local_folders = {}
+        self.local_files = {}
+        self.remote_folders = {}
+        self.remote_files = {}
     
-    def create_dfolder(self, child_name=None, parent_id=None):
-        if parent_id is None:
-            parent_id = self.root_id
-        if child_name is None:
-            child_name = "New Folder"
-        if self.cipher: child_name = obfuscate(self.password, child_name)
-        child_folder = self.drive.CreateFile({'title': child_name, 
-                                              'parents':[{'id':parent_id}], 
-                                              'mimeType' : 'application/vnd.google-apps.folder'})
-        child_folder.Upload()
-        return child_folder['id']
-
-    def search_droot(self, folder):
-        fileList = self.drive.ListFile({'q': "'root' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
-        return [{'title': f['title'], 'id': f['id']} for f in fileList if folder in f['title']]
-
-    def driveListFolder(self, parent, level=0, verbose=True, indent = "|---", decipher=False):
-        filelist=[]
-        file_list = self.drive.ListFile({'q': f"'{parent}' in parents and trashed=false"}).GetList()
-        for f in file_list:
-            title = f['title']
-
-            if f['mimeType']=='application/vnd.google-apps.folder': # if folder
-                if decipher: title = deobfuscate(self.password, title)
-                if verbose: print(f"{indent*level}{parent}: FOLDER {title}")
-                filelist.append({"id": f['id'],
-                                "isfolder": True,
-                                "title": title,
-                                "list": self.driveListFolder(f['id'], level+1, verbose, indent, decipher)
-                                })
-            else:
-                if decipher: title = deobfuscate(self.password, strip_aes(title))
-                if verbose: print(f"{indent*level}{parent}: FILE   {title}")
-                filelist.append({"id": f["id"],
-                                "title": title, 
-                                "isfolder": False,
-                                })
-        if level != 0:
-            return filelist
-        else:
-            return driveList2localDict(filelist, self.root_id, "root", verbose=False)
+    def reset(self):
+        self.local_folders = {}
+        self.local_files = {}
+        self.remote_folders = {}
+        self.remote_files = {}
     
-    def localListFolder(self, parent, level=0, verbose=False,
-                        ignore=ignore):
-        cwd = os.getcwd()  # restore cwd after recursion is finished
-        os.chdir(parent) 
-        indent = '    '
-        folder_dict={"isfolder": True, 
-                    "title": parent,
-                    "children": []}
-        file_list = [entry for entry in os.listdir(".") if ignore(entry)]
-        for f in file_list:
-            if os.path.isdir(f): # if folder
-                if verbose: print(f"{indent*level}{parent}: folder {f}")
-                folder_dict['children'].append(self.localListFolder(f, level=level+1, verbose=verbose))
-            else:
-                if verbose: print(f"{indent*level}{parent}: file   {f}")
-                folder_dict['children'].append({"isfolder": False, 
-                                                "title": f})
 
-        os.chdir(cwd)
-        return folder_dict
+# d = DriveHandler("./mypydrive_1")
+def search_droot(drive, folder):
+    remote_folder_list = drive.ListFile({'q': "'root' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+    return [{'title': f['title'], 'id': f['id']} for f in remote_folder_list if folder in f['title']]
     
-    def encript_and_upload(self, fname, parent_id):
-        if parent_id is None:
-            parent_id = self.root_id
-        fIn = open(fname, "rb")
-        fCiph = io.BytesIO()
-        pyAesCrypt.encryptStream(fIn, fCiph, self.password, self.bufferSize)
-        
-        if self.cipher: fname = obfuscate(self.password, fname)
-        file = self.drive.CreateFile({'title': f"{fname}.aes", 
-                                      'parents':[{'id': parent_id}]
-                                     })
-        file.SetContentBinary(fCiph)
-        file.Upload() # Actual Upload
-        
-        fIn.close()
-        return file['id']
-
-    def decript_stream_and_save(self, drive_file_id):
-        fDec = io.BytesIO()
-        dfile = self.drive.CreateFile({'id': drive_file_id})
-        fname = strip_aes(dfile['title'])
-        if self.cipher: fname = deobfuscate(self.password, fname)
-
-        fCiph = io.BytesIO(dfile.GetContentBinary())
-        pyAesCrypt.decryptStream(fCiph, fDec, self.password, self.bufferSize, len(fCiph.getvalue()))
-        with open(fname, 'wb') as f:
-            f.write(fDec.getvalue())
     
-    def upload_folder_tree(self, tree, drive_id=None, level=0, verbose=True, indent = "|---"):
-        cwd = os.getcwd()  # restore cwd after recursion is finished
-        parent_folder = tree['title']
-        os.chdir(parent_folder) 
+## List local files
+def get_local_folders(self, verbose=False):
+    folder_list= {self.local_root: ""}
+    def localListFolder(path_structure, verbose=verbose):
+        local_folders = [d for d in os.listdir(path_structure) if os.path.isdir(os.path.join(path_structure, d))]
+        for f in local_folders:
+            if ignore(f):
+                if verbose: print(f"{path_structure}/{f}")
+                folder_list[f"{path_structure}/{f}"] = ""
+                localListFolder(f"{path_structure}/{f}", verbose=verbose)
             
-        for f in tree['children']:
-            if f['isfolder']:
-                f['id'] = self.create_dfolder(f['title'], drive_id)
-                if verbose: print(f"{indent*level}> {parent_folder}: FOLDER {f['title']} \t\t {f['id']}")
-                self.upload_folder_tree(f, f['id'], level+1, verbose, indent)
-            else:
-                f['id'] = self.encript_and_upload(f['title'], drive_id)
-                if verbose: print(f"{indent*level}> {parent_folder}: FILE {f['title']} \t\t {f['id']}")
-        
-        os.chdir(cwd)
-        return tree
-    
-    def download_folder_tree(self, fsave, tree, ignore=ignore, level=0, verbose=True, indent = "|---"):
-        cwd = os.getcwd()  # restore cwd after recursion is finished
-        parent_folder = tree['title']
-        os.chdir(fsave) 
-        
-        for f in tree['children']:
-            if f['isfolder']:
-                title = f['title']
-                # if self.cipher: title = deobfuscate(self.password, title)
-                mkdir(title)
-                if verbose: print(f"{indent*level}> {parent_folder}: FOLDER {title} \t\t {f['id']}")
-                self.download_folder_tree(f['title'], f, verbose=verbose, indent=indent, level=level+1)
-            else:
-                title = f['title']
-                # if self.cipher: title = deobfuscate(self.password, strip_aes(title))
-                self.decript_stream_and_save(f['id'])
-                if verbose: print(f"{indent*level}> {parent_folder}: FILE {title} \t\t {f['id']}")
-        
-        os.chdir(cwd)
+    localListFolder(self.local_root, verbose=verbose)
+    self.local_folders = folder_list
 
-    def download_all(self, fsave, verbose=True):
-        cwd = os.getcwd()  # restore cwd after recursion is finished
-        try:
-            remote = self.driveListFolder(self.root_id, decipher=True, verbose=verbose)
-            self.download_folder_tree(fsave, remote)
-            return remote
-        except KeyboardInterrupt:
-            os.chdir(cwd) 
+def get_local_files(self):
+    lfile_list = {}
+    for fpath, _ in self.local_folders.items():
+        lfile_list[fpath] = {d: "" for d in os.listdir(fpath) if ignore(d) and os.path.isfile(os.path.join(fpath, d))}
+    self.local_files = lfile_list
+
+## List remote
+def get_remote_folders(self, decipher=True, verbose=True):
+    folder_list= {"remote_root": self.root_id}
+    if verbose: print(f"remote_root: '{self.root_id}'")
     
+    def driveListFolder( parent, path_structure, verbose=verbose, decipher=decipher):
+        remote_folders = self.drive.ListFile({'q': f"'{parent}' in parents and trashed=false and mimeType='application/vnd.google-apps.folder'"}).GetList()
+        for f in remote_folders:
+            title = f['title']
+            if decipher: title = deobfuscate(self.password, title)
+            if verbose: print(f"{path_structure}/{title}: {f['id']}")
+            folder_list[f"{path_structure}/{title}"] = f['id']
+            driveListFolder(f['id'], f"{path_structure}/{title}", verbose, decipher)
+        
+    driveListFolder(self.root_id, "remote_root", decipher=decipher, verbose=verbose)
+    self.remote_folders = folder_list
+
+def get_remote_files(self, verbose=False):
+    r_files = {}
+    decipher = True
+    for folder_name, folder_id in self.remote_folders.items():
+        files = {}
+        remote_files = self.drive.ListFile({'q': f"'{folder_id}' in parents and trashed=false and mimeType!='application/vnd.google-apps.folder'"}).GetList()
+        for f in remote_files:
+            title = f['title']
+            if decipher: title = deobfuscate(self.password, strip_aes(title))
+            files[f"{title}"] = f['id']
+            if verbose: print(f'REMOTE LS: {folder_name} {title}')
+        r_files[folder_name] = files
+    self.remote_files = r_files
+
+def _folders_to_create(self, verbose=False):
+    to_create = []
+    for folder in self.local_folders:
+        rf = folder.replace(self.local_root, "remote_root")
+        if rf not in self.remote_folders.keys():
+            if verbose: print(f"TODO REMOTE CREATE: {rf}" )
+            to_create.append(rf)
+    return to_create
+
+def create_remote_folder(self, cipher=True, verbose=True):
+    to_create = _folders_to_create(self, verbose=verbose)
+    for f in to_create:
+        dirs = f.split("/")
+        n = len(dirs)
+        for i in range(n):
+            remote_path = '/'.join(dirs[:i+1])
+            if remote_path in self.remote_folders.keys():
+                continue
+            else:
+                parent_id = self.remote_folders['/'.join(dirs[:i])]
+                if cipher: child_name = obfuscate(self.password, dirs[i])
+                child_folder = self.drive.CreateFile({'title': child_name, 
+                                                  'parents':[{'id':parent_id}], 
+                                                  'mimeType' : 'application/vnd.google-apps.folder'})
+                child_folder.Upload()
+                self.remote_folders[remote_path] = child_folder['id']
+                if verbose: print(f"REMOTE CREATE FOLDER: {remote_path} | {child_name} | p:{parent_id} | c:{child_folder['id']}" )
+
+def encript_and_upload(self, fpath, fname, parent_id):
+    fIn = open(f"{fpath}/{fname}", "rb")
+    fCiph = io.BytesIO()
+    pyAesCrypt.encryptStream(fIn, fCiph, self.password, self.bufferSize)
+
+    if self.cipher: fname = obfuscate(self.password, fname)
+    file = self.drive.CreateFile({'title': f"{fname}.aes", 
+                              'parents':[{'id': parent_id}]
+                                 })
+    file.SetContentBinary(fCiph)
+    file.Upload() # Actual Upload
+
+    fIn.close()
+    return file['id']
+
+def files_to_upload(self, verbose=False):
+    to_upload = []
+    for folder, files in self.local_files.items():
+        lf = folder
+        rf = folder.replace(self.local_root, "remote_root")
+        items = self.remote_files.get(rf , {})
+        
+        for f in files:
+            if f not in items.keys():
+                fsize = file_size(f'{lf}/{f}', in_bytes=True)
+#                 if fsize > 100*2**20:  # > 100Mb
+#                     continue
+                to_upload.append( (lf, f, self.remote_folders[rf]) )
+                if verbose: print(f"TO UPLOAD: {lf}/{f} | Remote Folder ID: {self.remote_folders[rf]}")
+                
+    return to_upload
+
+
+def upload_files(self, f2upload, verbose=False):
+    total = size_to_upload(f2upload, in_bytes=True)
+    print(f'Total to upload: {file_size(total)}')
+    for fpath, fname, parent_id in f2upload:
+        fsize = file_size(f'{fpath}/{fname}')
+        print(f"{file_size(total):8} | ({fsize}) {fname} | {fpath}", end="")
+        file_id = encript_and_upload(self, fpath, fname, parent_id)
+        print(f" | {file_id}")
+        
+        r_path = fpath.replace(self.local_root, "remote_root") 
+        rfiles = self.remote_files.get(r_path, {})
+        rfiles[fname] = file_id
+        self.remote_files[r_path] = rfiles
+        
+        total -= file_size(f'{fpath}/{fname}', in_bytes=True)
